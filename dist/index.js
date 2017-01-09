1c85a3620a06b4a0b11b64d50f1c4aa8a12c9b0f
Pablo Varela <pablovarela182@gmail.com>
2017-01-08 19:25:51 -0500

    Replace child_process with shelljs

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,19 @@
 'use strict';
 
-var _child_process = require('child_process');
+var _shelljs = require('shelljs');
 
-var _child_process2 = _interopRequireDefault(_child_process);
+var _shelljs2 = _interopRequireDefault(_shelljs);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function get(callback) {
-  (0, _child_process2.default)('ioreg -l | grep Capacity | cut -d\' \' -f19', function (error, stdout) {
-    if (error !== null) {
-      console.log('exec error: ' + error);
+  _shelljs2.default.exec('ioreg -l | grep Capacity | cut -d\' \' -f19', { silent: true }, function (code, stdout, stderr) {
+    if (code) {
+      console.log('exec error: ' + stderr);
       return null;
     }
 
-    var lines = stdout.match(/[^\r\n]+/g);
+    var lines = stdout.match(/[^\n]+/g);
     var capacityNow = parseInt(lines[0], 10);
     var capacityOriginal = parseInt(lines[3], 10);
     var health = capacityNow * 100 / capacityOriginal;
--- source/index.js
+++ source/index.js
@@ -1,13 +1,13 @@
-import exec from 'child_process'
+import shell from 'shelljs'
 
 function get(callback) {
-  exec('ioreg -l | grep Capacity | cut -d\' \' -f19', (error, stdout) => {
-    if (error !== null) {
-      console.log('exec error: ' + error)
+  shell.exec('ioreg -l | grep Capacity | cut -d\' \' -f19', {silent: true}, (code, stdout, stderr) => {
+    if (code) {
+      console.log('exec error: ' + stderr)
       return null
     }
 
-    const lines = stdout.match(/[^\r\n]+/g)
+    const lines = stdout.match(/[^\n]+/g)
     const capacityNow = parseInt(lines[0], 10)
     const capacityOriginal = parseInt(lines[3], 10)
     const health = (capacityNow * 100) / capacityOriginal
