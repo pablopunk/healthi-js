4d75a3ccd7b09004e13cc470b9172b8e7bebc723
Pablo Varela <pablovarela182@gmail.com>
2017-05-11 21:57:53 -0400

    Get rid of babel. YOLO

--- bin/healthi.js
+++ bin/healthi.js
@@ -1,50 +1,41 @@
-'use strict';
+const shell = require('shelljs')
 
-var _shelljs = require('shelljs');
+const command = 'ioreg'
+const options = ' -l | grep Capacity | cut -d\' \' -f19'
 
-var _shelljs2 = _interopRequireDefault(_shelljs);
+const commandExists = command => {
+  return shell.which(command)
+}
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var command = 'ioreg';
-var options = ' -l | grep Capacity | cut -d\' \' -f19';
-
-var commandExists = function commandExists(command) {
-  return _shelljs2.default.which(command);
-};
-
-var get = async function get() {
-  var battery = {
+const get = async () => {
+  let battery = {
     currentCapacity: 0,
     originalCapacity: 0,
     health: 0
-  };
+  }
   if (!commandExists(command)) {
-    throw new Error(command + ' command does not exist');
+    throw new Error(`${command} command does not exist`)
   }
 
-  var _shell$exec = _shelljs2.default.exec(command + options, { silent: true }),
-      code = _shell$exec.code,
-      stdout = _shell$exec.stdout,
-      stderr = _shell$exec.stderr;
+  const {code, stdout, stderr} = shell.exec(command + options, {silent: true})
 
   if (code) {
-    throw new Error(stderr);
+    throw new Error(stderr)
   }
 
-  var lines = stdout.match(/[^\n]+/g);
+  const lines = stdout.match(/[^\n]+/g)
   if (lines === null) {
-    throw new Error('error parsing "' + stdout + '"');
+    throw new Error(`error parsing "${stdout}"`)
   }
 
-  var capacityNow = parseInt(lines[0], 10);
-  var capacityOriginal = parseInt(lines[3], 10);
+  const capacityNow = parseInt(lines[0], 10)
+  const capacityOriginal = parseInt(lines[3], 10)
   battery = {
     currentCapacity: capacityNow,
     originalCapacity: capacityOriginal,
-    health: capacityNow * 100 / capacityOriginal
-  };
-  return battery;
-};
+    health: (capacityNow * 100) / capacityOriginal
+  }
+  return battery
+}
 
-module.exports = get;
+module.exports = get
