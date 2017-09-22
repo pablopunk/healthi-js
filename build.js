e9bd22591f70dea5963103edbf471f2f72feb6e1
Pablo Varela <pablovarela182@gmail.com>
2017-09-23 01:24:25 +0200

    babel-preset-env

--- build.js
+++ build.js
@@ -1,35 +1,32 @@
 'use strict';
 
-var shell = require('shelljs');
+const shell = require('shelljs');
 
-var command = 'ioreg';
-var options = ' -l | grep Capacity | cut -d\' \' -f19';
+const command = 'ioreg';
+const options = ' -l | grep Capacity | cut -d\' \' -f19';
 
-var commandExists = function commandExists(command) {
+const commandExists = command => {
   return shell.which(command);
 };
 
-var get = async function get() {
+const get = async () => {
   if (!commandExists(command)) {
-    throw new Error(command + ' command does not exist');
+    throw new Error(`${command} command does not exist`);
   }
 
-  var _shell$exec = shell.exec(command + options, { silent: true }),
-      code = _shell$exec.code,
-      stdout = _shell$exec.stdout,
-      stderr = _shell$exec.stderr;
+  const { code, stdout, stderr } = shell.exec(command + options, { silent: true });
 
   if (code) {
     throw new Error(stderr);
   }
 
-  var lines = stdout.match(/[^\n]+/g);
+  const lines = stdout.match(/[^\n]+/g);
   if (lines === null) {
-    throw new Error('error parsing "' + stdout + '"');
+    throw new Error(`error parsing "${stdout}"`);
   }
 
-  var capacityNow = parseInt(lines[0], 10);
-  var capacityOriginal = parseInt(lines[3], 10);
+  const capacityNow = parseInt(lines[0], 10);
+  const capacityOriginal = parseInt(lines[3], 10);
   return {
     currentCapacity: capacityNow,
     originalCapacity: capacityOriginal,
