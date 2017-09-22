623d34ca6c3c0d9e3f8aee331cc12e8ff6892e92
Pablo Varela <pablovarela182@gmail.com>
2017-09-23 01:27:37 +0200

    Electron preset

--- build.js
+++ build.js
@@ -1,5 +1,7 @@
 'use strict';
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 const shell = require('shelljs');
 
 const command = 'ioreg';
@@ -9,29 +11,35 @@ const commandExists = command => {
   return shell.which(command);
 };
 
-const get = async () => {
-  if (!commandExists(command)) {
-    throw new Error(`${command} command does not exist`);
-  }
+const get = (() => {
+  var _ref = _asyncToGenerator(function* () {
+    if (!commandExists(command)) {
+      throw new Error(`${command} command does not exist`);
+    }
 
-  const { code, stdout, stderr } = shell.exec(command + options, { silent: true });
+    const { code, stdout, stderr } = shell.exec(command + options, { silent: true });
 
-  if (code) {
-    throw new Error(stderr);
-  }
+    if (code) {
+      throw new Error(stderr);
+    }
 
-  const lines = stdout.match(/[^\n]+/g);
-  if (lines === null) {
-    throw new Error(`error parsing "${stdout}"`);
-  }
+    const lines = stdout.match(/[^\n]+/g);
+    if (lines === null) {
+      throw new Error(`error parsing "${stdout}"`);
+    }
 
-  const capacityNow = parseInt(lines[0], 10);
-  const capacityOriginal = parseInt(lines[3], 10);
-  return {
-    currentCapacity: capacityNow,
-    originalCapacity: capacityOriginal,
-    health: capacityNow * 100 / capacityOriginal
+    const capacityNow = parseInt(lines[0], 10);
+    const capacityOriginal = parseInt(lines[3], 10);
+    return {
+      currentCapacity: capacityNow,
+      originalCapacity: capacityOriginal,
+      health: capacityNow * 100 / capacityOriginal
+    };
+  });
+
+  return function get() {
+    return _ref.apply(this, arguments);
   };
-};
+})();
 
 module.exports = get;
