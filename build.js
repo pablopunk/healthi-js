ac91172b6b3623ffd99287df36e0dc6e78c9b3b0
Pablo Varela <pablovarela182@gmail.com>
2017-12-19 17:21:12 +0100

    Divide in platforms

--- build.js
+++ build.js
@@ -1,45 +1,53 @@
 'use strict';
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
-const execa = require('execa');
-
-const command = 'ioreg -rc AppleSmartBattery | grep .*Capacity.* | cut -d\' \' -f9';
-
-const commandExists = (() => {
-  var _ref = _asyncToGenerator(function* (command) {
-    const { code } = yield execa.shell(`which ${command}`);
-    return code === 0;
-  });
-
-  return function commandExists(_x) {
-    return _ref.apply(this, arguments);
-  };
-})();
-
-const get = (() => {
-  var _ref2 = _asyncToGenerator(function* () {
-    if (!commandExists(command)) {
-      throw new Error(`${command} command does not exist`);
+let getBattery = (() => {
+  var _ref = _asyncToGenerator(function* (os) {
+    if (!(os in commands)) {
+      throw new Error(`OS not supported (${os})`);
     }
 
-    const { code, stdout, stderr } = yield execa.shell(command);
-
+    const { code, stdout, stderr } = yield execa.shell(commands[os]);
     if (code) {
       throw new Error(stderr);
     }
 
-    const lines = stdout.match(/[^\n]+/g);
-    if (lines === null) {
-      throw new Error(`error parsing "${stdout}"`);
-    }
+    return parse[os](stdout);
+  });
 
-    const capacityNow = parseInt(lines[0], 10);
-    const capacityOriginal = parseInt(lines[3], 10);
+  return function getBattery(_x) {
+    return _ref.apply(this, arguments);
+  };
+})();
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+const os = require('os');
+const execa = require('execa');
+
+const commands = {
+  darwin: "ioreg -rc AppleSmartBattery | grep .*Capacity.* | cut -d' ' -f9",
+  linux: 'cat /sys/class/power_supply/BAT0/energy_full /sys/class/power_supply/BAT0/energy_full_design'
+};
+
+const parse = {
+  darwin: function (output) {
+    const lines = output.match(/[^\n]+/g);
+    return { now: parseInt(lines[0], 10), original: parseInt(lines[3], 10) };
+  },
+  linux: function (output) {
+    const lines = output.match(/[^\n]+/g);
+    return { now: parseInt(lines[0], 10), original: parseInt(lines[1], 10) };
+  }
+};
+
+const get = (() => {
+  var _ref2 = _asyncToGenerator(function* () {
+    const { now, original } = yield getBattery(os.platform());
+
     return {
-      currentCapacity: capacityNow,
-      originalCapacity: capacityOriginal,
-      health: capacityNow * 100 / capacityOriginal
+      now,
+      original,
+      health: now * 100 / original
     };
   });
 
--- index.js
+++ index.js
@@ -1,34 +1,43 @@
+const os = require('os')
 const execa = require('execa')
 
-const command = 'ioreg -rc AppleSmartBattery | grep .*Capacity.* | cut -d\' \' -f9'
-
-const commandExists = async command => {
-  const { code } = await execa.shell(`which ${command}`)
-  return code === 0
+const commands = {
+  darwin: "ioreg -rc AppleSmartBattery | grep .*Capacity.* | cut -d' ' -f9",
+  linux:
+    'cat /sys/class/power_supply/BAT0/energy_full /sys/class/power_supply/BAT0/energy_full_design'
 }
 
-const get = async () => {
-  if (!commandExists(command)) {
-    throw new Error(`${command} command does not exist`)
+const parse = {
+  darwin: function (output) {
+    const lines = output.match(/[^\n]+/g)
+    return { now: parseInt(lines[0], 10), original: parseInt(lines[3], 10) }
+  },
+  linux: function (output) {
+    const lines = output.match(/[^\n]+/g)
+    return { now: parseInt(lines[0], 10), original: parseInt(lines[1], 10) }
+  }
+}
+
+async function getBattery (os) {
+  if (!(os in commands)) {
+    throw new Error(`OS not supported (${os})`)
   }
 
-  const {code, stdout, stderr} = await execa.shell(command)
-
+  const { code, stdout, stderr } = await execa.shell(commands[os])
   if (code) {
     throw new Error(stderr)
   }
 
-  const lines = stdout.match(/[^\n]+/g)
-  if (lines === null) {
-    throw new Error(`error parsing "${stdout}"`)
-  }
+  return parse[os](stdout)
+}
 
-  const capacityNow = parseInt(lines[0], 10)
-  const capacityOriginal = parseInt(lines[3], 10)
+const get = async () => {
+  const { now, original } = await getBattery(os.platform())
+
   return {
-    currentCapacity: capacityNow,
-    originalCapacity: capacityOriginal,
-    health: (capacityNow * 100) / capacityOriginal
+    now,
+    original,
+    health: now * 100 / original
   }
 }
 
