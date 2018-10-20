48ce9c7498d064be266b7983dfe7787c0cbfad25
pablopunk <pablovarela182@gmail.com>
2018-10-20 02:57:16 +0200

    Remove babel and don't use async/await

---
+++ index.js
@@ -0,0 +1,28 @@
+const os = require('os')
+const execa = require('execa')
+const { canRun, parse, commands } = require('./lib/battery')
+
+function getBattery (os) {
+  if (!(os in commands)) {
+    throw new Error(`OS not supported (${os})`)
+  }
+
+  if (!canRun[os]()) {
+    return new Promise((resolve, reject) => resolve({now: 100, original: 100}))
+  }
+
+  return execa.shell(commands[os])
+    .then(({ code, stdout, stderr }) => {
+      if (code) {
+        throw new Error(stderr)
+      }
+
+      return parse[os](stdout)
+    })
+}
+
+const get = () =>
+  getBattery(os.platform())
+    .then(({ now, original }) => ({now, original, health: now * 100 / original}))
+
+module.exports = get
